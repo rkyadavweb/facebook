0c52232578e5dd0a1c09d3661c9ddd541e9e41e1
rkyadavweb <rkyadavbya@gmail.com>
2024-12-19 13:20:33 +0530

    Facebook

---
+++ script.js
@@ -0,0 +1,38 @@
+var settingmenu = document.querySelector(".setting-menu");
+
+function SettingmenuToggle(){
+    settingmenu.classList.toggle("setting-menu-height");
+
+}
+
+var notificationmenu = document.querySelector(".notification");
+
+function notificationMenuToggle(){
+    notificationmenu.classList.toggle("setting-menu-height");
+}
+
+var massengermenu = document.querySelector(".massenger-menu")
+
+function MassengermenuToggle(){
+    massengermenu.classList.toggle("setting-menu-height");
+}
+
+var moremenu = document.querySelector(".more-menu");
+
+function MoremenuToggle(){
+    moremenu.classList.toggle("setting-menu-height");
+
+}
+var profilepage1 = document.querySelector(".profile-page");
+
+function profilepageToggle(){
+    profilepage1.classList.toggle("profile-page-height");
+}
+var findPage = document.querySelector(".find-friend-page");
+function ffriendToggle(){
+    findPage.classList.toggle("profile-page-height")
+}
+var morepage = document.querySelector(".seemore-page");
+function moreToggle(){
+    morepage.classList.toggle("seemore-page-height");
+}
